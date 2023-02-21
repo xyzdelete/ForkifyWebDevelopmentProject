81a687543d87eb773653a8ef9d454d8a0b58f7e9
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-21 13:29:01 +0200

    Listening For load and hashchange Events

--- src/js/controller.js
+++ src/js/controller.js
@@ -33,11 +33,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
     try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+
         // 1) Loading recipe
         renderSpinner(recipeContainer);
         const res = await fetch(
-            // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40"
-            "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
         );
 
         const data = await res.json();
@@ -164,4 +166,4 @@ const showRecipe = async function () {
     }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
