3c369cc77cb45d64cd226673010b96866f871f36
Arturs Anikins <xyzdelete@protonmail.com>
2023-03-01 16:29:35 +0200

    Developing a DOM Updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,11 +12,13 @@ import recipeView from "./views/recipeView.js";
 
 const controlRecipes = async function () {
     try {
-        recipeView.renderSpinner();
-
         const id = window.location.hash.slice(1);
 
         if (!id) return;
+        recipeView.renderSpinner();
+
+        // 0) Update results view to mark selected search result
+        resultsView.update(model.getSearchResultsPage());
 
         // 1) Loading recipe
         await model.loadRecipe(id);
@@ -58,7 +60,8 @@ const controlServings = function (newServings) {
     model.updateServings(newServings);
 
     // Update the recipe view
-    recipeView.render(model.state.recipe);
+    // recipeView.render(model.state.recipe);
+    recipeView.update(model.state.recipe);
 };
 
 const init = function () {
