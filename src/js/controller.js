62e1f7f7e1371fd7a92f79f6c96d8b6fb3669a03
Arturs Anikins <xyzdelete@protonmail.com>
2023-03-01 15:50:15 +0200

    Updating Recipe Servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -53,8 +53,17 @@ const controlPagination = function (goToPage) {
     paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+    // Update the recipe servings (in state)
+    model.updateServings(newServings);
+
+    // Update the recipe view
+    recipeView.render(model.state.recipe);
+};
+
 const init = function () {
     recipeView.addHandlerRender(controlRecipes);
+    recipeView.addHandlerUpdateServings(controlServings);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
 };
