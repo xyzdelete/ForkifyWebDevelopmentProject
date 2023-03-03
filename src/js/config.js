7b5a9857e8b93005815b2823e0ec1a83dfb52de1
Arturs Anikins <xyzdelete@protonmail.com>
2023-03-03 16:03:43 +0200

    Set API key

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 // You need generate and set your own API key
 // from forkify-api.herokuapp.com/v2
-export const KEY = "";
+export const KEY = "5aec1b77-234d-4990-9a5e-66a596915ef0";
 export const MODAL_CLOSE_SEC = 2.5;
